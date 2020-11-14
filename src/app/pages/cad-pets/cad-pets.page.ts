0964a5f9fb867959a5b9d46a7f289fff3c0c3acf
Peter Lange Oliveira <44213765+Peter-loliveira@users.noreply.github.com>
2020-11-14 10:46:14 -0300

    Ajuste da opção de Namoro do PET
    Na tela de Cadastro de Pet coloquei um ion-toggle na opção de namoro e ajustei no HTML e no .ts

--- src/app/pages/cad-pets/cad-pets.page.ts
+++ src/app/pages/cad-pets/cad-pets.page.ts
@@ -1,31 +1,36 @@
-import { Pets } from 'src/models/Pets';
-import { Component, OnInit } from '@angular/core';
-import { PetsService } from 'src/app/services/PetsService';
+import { Pets } from "src/models/Pets";
+import { Component, OnInit } from "@angular/core";
+import { PetsService } from "src/app/services/PetsService";
 
 @Component({
-  selector: 'app-cad-pets',
-  templateUrl: './cad-pets.page.html',
-  styleUrls: ['./cad-pets.page.scss'],
+  selector: "app-cad-pets",
+  templateUrl: "./cad-pets.page.html",
+  styleUrls: ["./cad-pets.page.scss"],
 })
 export class CadPetsPage implements OnInit {
-
-    public pet: Pets = new Pets()
-  constructor(
-    private _petService: PetsService
-  ) { }
-
-  ngOnInit() {
+  public pet: Pets = new Pets();
+  public escolhaNamoro
+  
+  constructor(private _petService: PetsService) {
+    this.pet.namoro = 'N'
   }
+
+  ngOnInit() {}
 
   cadastrarPet() {
-    console.log(this.pet)
-    this._petService.cadastrar(this.pet).subscribe( res => {
+    console.log(this.pet);
+    this._petService.cadastrar(this.pet).subscribe((res) => {
       console.log(res);
-    })
-      
-    let mensagemAlerta: string = 'Cadastro realizado com sucesso.'
-    alert(mensagemAlerta)
+    });
 
+    let mensagemAlerta: string = "Cadastro realizado com sucesso.";
+    alert(mensagemAlerta);
   }
 
+  namoro(){
+    this.escolhaNamoro = document.getElementById('namoro');
+    if (this.escolhaNamoro.checked) {
+      this.pet.namoro = 'S'
+    } else {this.pet.namoro = 'N'}
+  }
 }
--- src/app/services/PetsService.ts
+++ src/app/services/PetsService.ts
@@ -1,51 +1,53 @@
-import {IPetsService} from 'src/app/interfaces/IPetsService';
-import { Usuario } from 'src/models/Usuario';
-import { HttpClient } from '@angular/common/http';
-import { Pets} from 'src/models/Pets';
-import { Observable } from 'rxjs';
-import { Injectable } from '@angular/core';
-import { Global } from 'src/theme/shared/Global';
+import { IPetsService } from "src/app/interfaces/IPetsService";
+import { Usuario } from "src/models/Usuario";
+import { HttpClient } from "@angular/common/http";
+import { Pets } from "src/models/Pets";
+import { Observable } from "rxjs";
+import { Injectable } from "@angular/core";
+import { Global } from "src/theme/shared/Global";
 
-import { UsuarioService } from 'src/app/services/UsuarioService';
+import { UsuarioService } from "src/app/services/UsuarioService";
 
 @Injectable({
-    'providedIn': 'root'
+  providedIn: "root",
 })
-export class PetsService implements IPetsService{
-   
-    public apiUrl: string = Global.ApiUrl + "pets";
-    private _usuarioLogado: Usuario = new Usuario()
+export class PetsService implements IPetsService {
+  public apiUrl: string = Global.ApiUrl + "pets";
+  private _usuarioLogado: Usuario = new Usuario();
 
-    constructor( private _usuarioServices: UsuarioService, private _http: HttpClient ) {
+  constructor(
+    private _usuarioServices: UsuarioService,
+    private _http: HttpClient
+  ) {
+    this._usuarioLogado = this._usuarioServices.retornarUsuarioLogado();
+  }
 
-        this._usuarioLogado = this._usuarioServices.retornarUsuarioLogado()
-    }
+  cadastrar(pets: Pets): Observable<Pets> {
+    if (!pets.nome) throw new Error("O campo Nome é obrigatorio.");
+    if (!pets.nascimento) throw new Error("O campo Data de nascimento é obrigatorio.");
+    if (!pets.tipo) throw new Error("O campo Tipo é obrigatorio.");
+    if (!pets.raca) throw new Error("O campo Raca é obrigatorio.");
+    if (!pets.sexo) throw new Error("O campo Sexo é obrigatorio.");
+    pets.usuario_id = this._usuarioLogado.id;
 
-    cadastrar(pets: Pets): Observable<Pets> {
+    console.log(pets);
 
-        if (!pets.nome) throw new Error ('O campo Nome é obrigatorio.')
-        if (!pets.nascimento) throw new Error ('O campo Data de nascimento é obrigatorio.')
-        if (!pets.tipo) throw new Error ('O campo Tipo é obrigatorio.')
-        if (!pets.raca) throw new Error ('O campo Raca é obrigatorio.')
-        if (!pets.sexo) throw new Error ('O campo Sexo é obrigatorio.')
-        if (!pets.namoro) throw new Error ('Informe se ele está disponivel para namoros')
-        pets.usuario_id = this._usuarioLogado.id
+    return this._http.post<Pets>(this.apiUrl, pets);
+  }
 
-        console.log(pets);
+  remover(Pets_id: number): void {
+    throw new Error("Method not implemented.");
+  }
 
-        return this._http.post<Pets>(this.apiUrl, pets)
-    }
-    remover(Pets_id: number): void {
-        throw new Error("Method not implemented.");
-    }
-    editar(pets: Pets): Observable<Pets> {
-        throw new Error("Method not implemented.");
-    }
-    listar(): Observable<Pets[]> {
-        throw new Error("Method not implemented.");
-    }
-    buscar(pets_id: number): Observable<Pets> {
-        throw new Error("Method not implemented.");
-    }
-    
+  editar(pets: Pets): Observable<Pets> {
+    throw new Error("Method not implemented.");
+  }
+
+  listar(): Observable<Pets[]> {
+    throw new Error("Method not implemented.");
+  }
+
+  buscar(pets_id: number): Observable<Pets> {
+    throw new Error("Method not implemented.");
+  }
 }
