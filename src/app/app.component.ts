5a47e64f13be279735a4715ef27654dfcac77bc0
victorssoares <60630624+victorssoares@users.noreply.github.com>
2020-11-22 16:17:14 -0300

    Firebase chat
    chat usando firebase

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,24 @@
 import { Component } from '@angular/core';
-
 import { Platform } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
+import firebase from 'firebase';
+import {ChatPage} from '../app/pages/chat/chat.page'
+
+
+
+
+const firebaseConfig = {
+  apiKey:"AIzaSyAsg4L40-BRArQfR-aFH-Mx8JmT1OM1f7g",
+  authDomain:"pet-perfeito-22335.firebaseapp.com",
+  databaseURL:"https://pet-perfeito-22335.firebaseio.com",
+  projectId:"pet-perfeito-22335",
+  storageBucket:"pet-perfeito-22335.appspot.com",
+  messagingSenderId:"701284366744",
+  appId:"1:701284366744:web:e9c660730ca176191593ed",
+  measurementId:"G-BY0D7VVBEP"
+};
+
 
 @Component({
   selector: 'app-root',
@@ -10,18 +26,14 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
   styleUrls: ['app.component.scss']
 })
 export class AppComponent {
-  constructor(
-    private platform: Platform,
-    private splashScreen: SplashScreen,
-    private statusBar: StatusBar
-  ) {
-    this.initializeApp();
-  }
-
-  initializeApp() {
-    this.platform.ready().then(() => {
-      this.statusBar.styleDefault();
-      this.splashScreen.hide();
+  rootPage:any = ChatPage;
+  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
+    platform.ready().then(() => {
+      // Okay, so the platform is ready and our plugins are available.
+      // Here you can do any higher level native things you might need.
+      statusBar.styleDefault();
+      splashScreen.hide();
     });
+   firebase.initializeApp(firebaseConfig);
   }
 }
--- src/app/pages/chat/chat.page.ts
+++ src/app/pages/chat/chat.page.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import  firebase from 'firebase';
+import { UsuarioService } from 'src/app/services/UsuarioService';
+import { Usuario } from 'src/models/Usuario';
 
 @Component({
   selector: 'app-chat',
@@ -6,8 +9,42 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./chat.page.scss'],
 })
 export class ChatPage implements OnInit {
+  // @ViewChild("content") content: any;
+  userName: string = "";
+  mensagem: string = "";
+  mensagens = [];
+  private usuarioLogado: Usuario = new Usuario();
+  
+  constructor(private _usuarioService: UsuarioService) { 
+    this.getMessages();
+    this.usuarioLogado= this._usuarioService.retornarUsuarioLogado();
+    console.log(this.usuarioLogado);
+  }
+  
+  getMessages(){
+    var messagesRef = firebase.database().ref().child("mensagens");
+    messagesRef.on("value", (snap) => {
+      var data = snap.val();
+      this.mensagens = [];
+      for(var key in data){
+        this.mensagens.push(data[key]);
+      }
 
-  constructor() { }
+      // this.scrollToBottom();
+    });
+  }
+
+  // scrollToBottom(){
+  //   var contentEnd = document.getElementById("content-end").offsetTop;
+  //   this.content.scrollTo(0, contentEnd, 300);
+  // }
+  sendMessage(){
+    var messagesRef = firebase.database().ref().child("mensagens");
+    messagesRef.push({mensagem: this.mensagem, nome: this.userName });
+    this.mensagem = "";
+  }
+
+
 
   ngOnInit() {
   }
