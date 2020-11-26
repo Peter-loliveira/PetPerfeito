02431bb4069e62131e738171c2e6a288a094c475
Peter Lange Oliveira <44213765+Peter-loliveira@users.noreply.github.com>
2020-11-26 10:08:47 -0300

    Hora Mensagem
    Inseridoi na lógica a hora em que a mensagem é enviada

--- src/app/pages/chat/chat.page.ts
+++ src/app/pages/chat/chat.page.ts
@@ -12,6 +12,7 @@ export class ChatPage implements OnInit {
   // @ViewChild("content") content: any;
   userName: string = "";
   mensagem: string = "";
+  horaMensagem: Date = null;
   mensagens = [];
   private usuarioLogado: Usuario = new Usuario();
   
@@ -39,9 +40,27 @@ export class ChatPage implements OnInit {
   //   this.content.scrollTo(0, contentEnd, 300);
   // }
   sendMessage(){
+    // Se não houver texto na mensagem não faça nada
+    if (!this.mensagem){
+      return
+    }
+    this.horaMensagem = new Date();
+
+    let minuto = this.horaMensagem.getMinutes();
+
+    //Verifica se o min é menor que 10 e se posito acrescemnta um 0 à esquerda
+    if (minuto < 10) {
+      var horaAtual = `${this.horaMensagem.getHours()}:0${minuto}`;
+    } else {
+      var horaAtual = `${this.horaMensagem.getHours()}:${minuto}`;
+    }
+
     this.userName = this.usuarioLogado.nome;
+
+    console.log(horaAtual);    
+
     var messagesRef = firebase.database().ref().child("mensagens");
-    messagesRef.push({mensagem: this.mensagem, nome: this.userName });
+    messagesRef.push({mensagem: this.mensagem, nome: this.userName, hora: horaAtual });
     this.mensagem = "";
     // this.scrollToBottom();
   }
