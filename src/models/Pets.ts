5301e9ba809774e60e3b639ee5c0478798edae99
Peter Lange Oliveira <44213765+Peter-loliveira@users.noreply.github.com>
2020-12-06 18:13:23 -0300

    Add Campo Sobre no Cad de Pets

--- src/models/Pets.ts
+++ src/models/Pets.ts
@@ -6,7 +6,6 @@ export class Pets{
         public raca: string = null;
         public sexo: string = null;
         public namoro: string=null;
+        public sobre: string=null;
         public usuario_id: number = null;
-    
-
 }
